267bb35927faaf57b3261dbe559a0052ee8d1bd2
Charles Moulliard <cmoulliard@apache.org>
2017-06-08 12:30:41 +0200

    Test successfully using @Component and @Provider annotations

--- workshop/cdservice/src/main/java/org/cdservice/rest/CorsFilter.java
+++ workshop/cdservice/src/main/java/org/cdservice/rest/CorsFilter.java
@@ -1,17 +1,9 @@
 package org.cdservice.rest;
 
-import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
 
 import org.springframework.stereotype.Component;
 
@@ -19,26 +11,10 @@ import org.springframework.stereotype.Component;
  * @author <a href="mailto:cmoullia@redhat.com">Charles Moulliard</a>
  */
 @Component
-public class CorsFilter implements Filter {
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
 
-	public void doFilter(ServletRequest request, ServletResponse res, FilterChain chain)
-			throws IOException, ServletException {
-		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Expose-Headers","Location");
-		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type, User-Agent, X-Requested-With, X-Requested-By, Cache-Control");
-		response.setHeader("Access-Control-Allow-Credentials","true");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		chain.doFilter(request, res);
-	}
-
-	public void init(FilterConfig filterConfig) {}
-
-	public void destroy() {}
-
-}
-/*	@Override
+	@Override
 	public void filter(ContainerRequestContext request,
 			ContainerResponseContext response) {
 		response.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
@@ -51,4 +27,5 @@ public class CorsFilter implements Filter {
 						"Content-Type, User-Agent, X-Requested-With, X-Requested-By, Cache-Control");
 		response.getHeaders().putSingle("Access-Control-Allow-Credentials",
 				"true");
-	}*/
+	}
+}
