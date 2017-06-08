daa494072a4977007d6aeddb0c17c075cd2e82c4
Charles Moulliard <cmoulliard@apache.org>
2017-06-08 17:03:10 +0200

    Update project

--- workshop/cdfront/src/main/resources/static/scripts/services/CatalogFactory.js
+++ workshop/cdfront/src/main/resources/static/scripts/services/CatalogFactory.js
@@ -1,4 +1,8 @@
-angular.module('cdservice').factory('CatalogResource', function($resource){
-    var resource = $resource('http://localhost:8080/rest/catalogs/:CatalogId',{CatalogId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
-    return resource;
+angular.module('cdservice').factory('CatalogResource', function ($resource, config) {
+  return $resource(config.getApiUrl() + ':CatalogId', { CatalogId: '@id' }, {
+    'queryAll': {
+      method: 'GET',
+      isArray: true
+    }, 'query': { method: 'GET', isArray: false }, 'update': { method: 'PUT' }
+  });
 });
