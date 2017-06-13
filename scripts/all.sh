c23dab0bf68cd4a5aae52008d5a958cac8ab038f
Charles Moulliard <cmoulliard@apache.org>
2017-06-13 13:57:38 +0200

    Fix error

--- scripts/all.sh
+++ scripts/all.sh
@@ -7,25 +7,33 @@
 # ./scripts/all.sh -a https://console.35.187.106.198.nip.io:8443 -t TOKEN
 #
 
-while getopts a:t:u:p: option
+while getopts aupt option
 do
         case "${option}"
         in
                 a) api=${OPTARG};;
-                t) token=${OPTARG};;
                 u) user=${OPTARG};;
                 p) password=${OPTARG};;
+                t) token=${OPTARG};;
         esac
 done
 
-api=${1:-$(minishift console --url)}
-user=${2:-admin}
-password=${3:-admin}
+if [ -z "$api" ];then
+  api=$(minishift console --url)
+fi
+
+if [ -z "$user" ];then
+  user="admin"
+fi
+
+if [ -z "$password" ];then
+  password="admin"
+fi
 
 echo "Log on to OpenShift Machine"
 if [ "$token" != "" ]; then
-   echo "oc login https://$api --token=$token"
-   oc login https://$api --token=$token
+   echo "oc login $api --token=$token"
+   oc login $api --token=$token
 else
    echo "oc login $api -u $user -p $password"
    oc login $api -u $user -p $password
@@ -33,8 +41,8 @@ fi
 
 oc delete project/workshop
 
-./scripts/create_cdstore.sh demo
-./scripts/deploy_on_openshift.sh
+#./scripts/create_cdstore.sh demo
+#./scripts/deploy_on_openshift.sh
 
 export APP=$(oc get route cdservice -o json | jq '.spec.host' | tr -d \"\")
 while [ $(curl --write-out %{http_code} --silent --output /dev/null $APP/rest/catalogs) != 200 ]
